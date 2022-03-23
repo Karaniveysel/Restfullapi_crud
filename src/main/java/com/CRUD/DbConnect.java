1e00562340fb04ec1cee05326c459ad2dfa0b25f
karaniveysel <veyselkaranikont@gmail.com>
2022-03-23 23:10:33 +0300

    crud-1

--- src/main/java/com/CRUD/DbConnect.java
+++ src/main/java/com/CRUD/DbConnect.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class DbConnectMainClass {
+public class DbConnect {
 
 	public static void main(String[] args) {
 //		Connection conn = null;
